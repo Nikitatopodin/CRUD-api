542b32eb5e1c14a54d9fac15c2696fd134a4ce8c
unknown <nikitatop1@gmail.com>
2024-02-08 12:02:33 +0300

    init: start CRDU-api task

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// import http from 'node:http';
+
+// const server = http.createServer((req, res) => {
+//   switch (true) {
+//     case (req.url === 'api/users' && req.method === 'GET'):
+//       res.writeHead(200, { 'Content-Type': 'application/json' });
+//       res.end('Hello World!');
+//     case: (req.url === 'api')
+//   }
+
+// });
+
+// server.listen(8000);
